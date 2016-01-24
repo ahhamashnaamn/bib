0ca6d9887e59db2d3408fc0271637e91c63fbd41
CCAAHH <ccaahh430@gmail.com>
2016-01-24 17:41:57 +0000

    Create fucker1.lua

---
+++ plugins/fucker1.lua
@@ -0,0 +1,22 @@
+do
+local function run(msg, matches)
+local bot_id = our_id 
+local fbotmain = 23456789 
+
+    if matches[1] == 'bye' and is_admin(msg) or msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
+       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+    elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(fbotmain) then
+       send_large_msg("chat#id"..msg.to.id, 'Madarjende rafighe mano Az Goroh pak mikoni???/nMadarjende rafighe mano Az Goroh pak mikoni???/nMadarjende rafighe mano Az Goroh pak mikoni???/n', ok_cb, false)
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..fbotmain, ok_cb, false)
+    end
+end
+ 
+return {
+  patterns = {
+    "^[!/](bye)$",
+    "^!!tgservice (.+)$",
+  },
+  run = run
+}
+end
+-- By Arian
